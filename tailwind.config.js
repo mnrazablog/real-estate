ecba173998cd76e09a5face1e1eac0999abc0ccc
mnrazablog <mnraza94@gmail.com>
2023-04-29 14:08:52 +0530

    Commit 2

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: "0.75rem",      // "12px",
+        base: "0.875rem",   // "14px",
+        md: "1rem",         // "16px"
+        lg: "1.25rem",      // "20px"
+        xl: "1.5rem",       // "24px"
+        "2xl": "1.75rem",   // "28px"
+        "3xl": "2.25rem",   // "36px"
+      },
+    },
+  },
+  plugins: [],
+};
+
